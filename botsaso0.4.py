80c6d28d397b2ada91940e7df35cf60e7e146218
AndreuCi <carola.cfg@gmail.com>
2025-08-27 18:49:25 -0600

    Create botsaso0.4.py

---
+++ botsaso0.4.py
@@ -0,0 +1,41 @@
+# This example requires the 'members' and 'message_content' privileged intents to function.
+import random
+import discord
+from discord.ext import commands
+import random
+
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=',',  intents=intents)
+
+
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+
+    await ctx.send(left + right)
+@bot.command()
+async def hola(ctx):
+    await ctx.send("hola me llamo botsaso")
+@bot.command()
+async def  pregunta(ctx, cantidad):
+    
+    cantidad = int(cantidad)
+    
+
+    letras = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    contraseña = "".join(random.choice(letras) for i in range(cantidad))
+    await ctx.send(f"tu contraseña es {contraseña}")
+@bot.command()
+async def google(ctx):
+    await ctx.send("https://www.google.com/")
+@bot.command()
+
+
+
+
+bot.run(')
